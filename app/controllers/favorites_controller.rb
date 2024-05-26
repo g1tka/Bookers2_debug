0c7cca02d52f172f6514cdf0c11484643c1dcf0d
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-26 00:45:26 +0000

    [Add]2-debug

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,16 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    if request.path == "/books"
+      redirect_to books_path
+    else
+      redirect_to book_path(book)
+    end
+#    if request.path.match?("/books/[0-9]+")正規表現
+      # redirect_to book_path(book)
+#    elsif request.path.match?("/books")
+      # redirect_to books_path
+    # end
   end
 
   def destroy
