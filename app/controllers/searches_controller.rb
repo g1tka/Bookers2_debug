d76db56f7654f1ef6e6526fb7ae42b6604aff4aa
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-29 07:36:28 +0000

    [Add]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,7 @@ class SearchesController < ApplicationController
   
   def search
     @range = params[:range]
+    @word = params[:word]
     
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,19 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = USer.all
+    end
+  end
+  
 end
