6d8da585aa19f0fcabc4843f664da2e8557c7624
g1tka <165782083+g1tka@users.noreply.github.com>
2024-05-31 13:26:30 +0000

    [Update]comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,29 +1,32 @@
 class BookCommentsController < ApplicationController
 
     def create
-      @book = Book.find(params[:book_id])
+      book = Book.find(params[:book_id])
       @book_comment = current_user.book_comments.new(book_comment_params)
       # comment = BookComment.new(book_comment_params)
       # comment.user_id = current_user.id この二つを↑一文で表せる。
-      @book_comment.book_id = @book.id
-
-      if @book_comment.save
-        redirect_to book_path(@book)
-      else
+      @book_comment.book_id = book.id
+      @book_comment.save
+      
+      # if @book_comment.save
+      #   redirect_to book_path(@book)
+      # else
         
-        @book_new = Book.new
-        @user = @book.user
+      #   @book_new = Book.new
+      #   @user = @book.user
+      #   render 'books/show', id: @book.id
         
-        render 'books/show', id: @book.id
         # redirect_to book_path(book, anchor: 'comment_form')
         # flash[:notice] = '!注意：空のコメントはできません！'
         # redirect_to book_path(book)
-      end
+      # end
     end
 
     def destroy
-      BookComment.find(params[:id]).destroy
-      redirect_to book_path(params[:book_id])
+      @book_comment = BookComment.find(params[:id])
+      @book_comment.destroy
+      # BookComment.find(params[:id]).destroy
+      # redirect_to book_path(params[:book_id])
     end
 
   private
